2cf726edfb446dfc6cdcb31772418a8308afa1f4
Nitesh kumar <30889903+nit3sh007@users.noreply.github.com>
2023-11-24 15:53:04 +0530

    main_app.py

--- main_app.py
+++ main_app.py
@@ -25,31 +25,37 @@ plant_image = st.file_uploader("Choose an image...", type="jpg")
 submit = st.button('Predict')
 
 #On predict button click
+# On predict button click
 if submit:
     if plant_image is not None:
-
         # Convert the file to an opencv image.
         file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
 
         # Check if the image size is greater than 256x256
         if opencv_image.shape[0] > 256 or opencv_image.shape[1] > 256:
-            st.error("Choose a valid image")
-        else:
-            # Displaying the image
-            st.image(opencv_image, channels="BGR")
-            st.write(opencv_image.shape)
-            
-            # Resizing the image
+            st.warning("Image size is greater than 256x256. Resizing...")
             opencv_image = cv2.resize(opencv_image, (256, 256))
-            
-            # Convert image to 4 Dimension
-            opencv_image.shape = (1, 256, 256, 3)
-            
-            # Make Prediction
-            Y_pred = model.predict(opencv_image)
-            result = CLASS_NAMES[np.argmax(Y_pred)]
-            st.title(str("This is "+result.split('-')[0]+ " leaf with " + result.split('-')[1]))
+
+        # Displaying the image
+        st.image(opencv_image, channels="BGR")
+        st.write(opencv_image.shape)
+
+        # Resizing the image
+        opencv_image = cv2.resize(opencv_image, (256, 256))
+
+        # Convert image to 4 Dimension
+        opencv_image = np.expand_dims(opencv_image, axis=0)
+
+        # Make Prediction
+        Y_pred = model.predict(opencv_image)
+        result_index = np.argmax(Y_pred)
+
+        # Check if the result index is within bounds
+        if 0 <= result_index < len(CLASS_NAMES):
+            result = CLASS_NAMES[result_index]
+            st.title(f"This is {result} leaf")
+        else:
+            st.warning("Invalid result index.")
     else:
         st.warning("Please upload an image before clicking Predict.")
-
